86018c80c27ae92e3f0ecef53960f3553503949d
James Conlon <jconlon2@uic.edu>
2017-02-28 04:19:44 -0600

    Add files via upload

---
+++ random_data.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 23 01:39:38 2017
+
+@author: James
+"""
+
+#necessary libraries
+import random
+import numpy as np
+from sklearn.cluster import KMeans
+from sklearn.datasets.samples_generator import make_blobs
+import matplotlib.pyplot as plt
+
+
+##########
+#feel free to change any of the following:
+n_clusters = 5 
+n_samples = 300
+cluster_std = 10
+method = 'random' # can be changed to 'k-means++'
+###########
+
+boundary = 100 #rough scope of the plot is boundary X boundary. This can be changed
+
+'''
+Don't change anything below here
+'''
+
+#randomizing the location of the clusters
+centroids= []
+for i in range(n_clusters):
+    xi = int(random.random()*boundary)
+    yi = int(random.random()*boundary)                     
+    centroids.append([xi,yi])
+    
+generated_centroids = np.reshape(centroids,(n_clusters,2))
+print(generated_centroids)
+
+
+#making random blobs around centroids
+X, x = make_blobs(n_samples=n_samples,centers = centroids,cluster_std=cluster_std)
+X_orig = np.column_stack((X,x))
+
+fig = plt.gcf()
+fig.set_size_inches(12,12)
+
+plt.scatter(X[:,0],X[:,1],color = 'k')
+plt.scatter(generated_centroids[:,0],generated_centroids[:,1],marker='D', color='b')
+
+colormap = np.array(['black','green','cyan','yellow','orange','magenta','brown']*10)
+
+test_means = KMeans(n_clusters = n_clusters,init=method) 
+test_means.fit(X)
+test_centers = test_means.cluster_centers_
+test_labels = test_means.labels_
+
+
+def getDist(x1,y1,x2,y2):
+    x = x1-x2
+    y= y1-y2
+    return((x**2+y**2)**.5)
+
+
+# making sure the new clusters are in the same order as the generated ones
+ref_array = [-1]*5
+for i in range(n_clusters):
+    dist_array = [1000]*20
+
+    temp_set = centroids[i]
+    temp_x = temp_set[0]
+    temp_y = temp_set[1]
+    temp_index = 0
+    temp_dist_array = []
+    for j in range(n_clusters):
+       test_set = test_centers[j]
+       test_x = test_set[0]
+       test_y = test_set[1]
+       temp_dist_array.append(getDist(temp_x,temp_y,test_x,test_y))
+    ref_array[i] = temp_dist_array.index(min(temp_dist_array))
+
+location_match = []
+for i in range(n_clusters):
+    location_match.extend(test_centers[ref_array[i]])
+    
+location_match = np.reshape(location_match,(n_clusters,2))
+final_test_centers = location_match
+print(final_test_centers.round(1))
+
+
+plt.scatter(X[:,0],X[:,1],c = colormap[test_labels])
+plt.scatter(final_test_centers[:,0],final_test_centers[:,1],marker = 'X', color = 'r')
+plt.show()
